697e7bfee1b15578056d4252243b22cf4676256c
towhid <niamaaktar@gmail.com>
2023-07-20 15:38:22 +0600

    form margin issue has fixed

--- src/app/Register/schemas/page.jsx
+++ src/app/Register/schemas/page.jsx
@@ -2,19 +2,19 @@ import * as yup from "yup";
 
 const passValidator = "^(?=.*[A-Za-z])(?=.*d)[A-Za-zd]{8,}$";
 export const formSchema = yup.object().shape({
-  fName: yup.string().required("enter your first name"),
-  lName: yup.string().required("enter your last name"),
+  fName: yup.string().required("first name is required"),
+  lName: yup.string().required("last name is required"),
   email: yup
     .string()
-    .email("please enter your email")
-    .required("field can't be empty"),
-  file: yup.mixed().required("Image need to be uploaded"),
+    .email("email is required")
+    .required("file is required"),
+  file: yup.mixed().required("Image field is required"),
   password: yup
     .string()
     // .matches(passValidator, {
     //   message: "Minimum eight characters, at least one letter and one number",
     // })
-    .required("enter your password"),
+    .required("password is required"),
 });
 
 export const productSchema = yup.object().shape({
@@ -22,7 +22,7 @@ export const productSchema = yup.object().shape({
   desc: yup.string().required("description is required"),
   price: yup.number().required("price is required"),
   category: yup.string().required("category is required"),
-  file: yup.mixed().required("Image need to be uploaded"),
+  file: yup.mixed().required("Image field is required"),
 });
 export const loginSchema = yup.object().shape({
   email: yup
